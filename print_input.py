67167daa66a3c54f4a216d3e23e83693151c347d
yuriiSw <yuriwsouza@outlook.com>
2024-08-26 18:03:52 -0300

    26/03 - 2

---
+++ print_input.py
@@ -0,0 +1,11 @@
+nome = input("informe seu nome: ")
+idade= input("Informe sua idade:" )
+print (nome)
+print (idade)
+
+print(nome, idade)
+print(nome, idade, end="... \n")
+print(nome, idade, sep="#", end="... \n")
+print(nome, idade, sep="#")
+
+print(5 // 2)
---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome= "Yuri"
+idade = 21
+
+nome,idade = "Aleatorio" ,00
+
+print (nome, idade)
+
+limite_saque_diario =100
+
+BR_STATES = ["SP","RJ","SC","RS"]
+
+print(BR_STATES)
